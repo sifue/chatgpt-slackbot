cf3fc6f560e4874897b3e54675b1112bdf5c537b
sifue <yoshimura@soichiro.org>
2022-12-06 22:50:09 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# ChatGPT Slackbot
+Slackを通じて会話人工知能のChatGPTを利用するためのBOT
+環境構築にはOpenAIのアカウント及びSlackのBoltのアプリケーショントークンが必要
+ライブラリには[acheong08/ChatGPT](https://github.com/acheong08/ChatGPT)を利用
+
+## ボットの使い方
+- AIとの会話: !gpt \[会話内容\]
+- 使い方を表示: !gpt-help
+
+## 環境構築
+### OpenAIのアカウントの用意 (ID/PASSWOARD形式)
+[OpenAI Chat](https://chat.openai.com/)にアクセスしてID/PASSWOARD形式でアカウント作成。
+
+### Slack Botのトークンの用意
+[Bolt 入門ガイド](https://slack.dev/bolt-python/ja-jp/tutorial/getting-started)に準拠。
+
+- SLACK_BOT_TOKEN
+- SLACK_APP_TOKEN
+
+を取得しておく。
+
+#### SLACK_BOT_TOKENで要求するスコープ
+
+- chat:write 
+
+#### Event SubscriptionsのSubscribe to Bot Events で要求するスコープ
+
+- message.channels
+- message.groups
+- message.im
+- message.mpim
+
+### インストール方法
+Python3.6.8以上で動作を確認済み。
+
+.env ファイルを実行フォルダ内に作成して以下を記述
+
+```sh
+pip3 install python-dotenv --upgrade
+pip3 install revChatGPT --upgrade
+pip install slack_bolt --upgrade
+pip3 list # python-dotenv と revChatGPT と slack-bolt と slack-sdkを確認
+python3 app.py
+```
+
+以上で起動。tmuxなどのセッションを維持するツールで起動することを前提としている。
---
+++ app.py
@@ -0,0 +1,45 @@
+from revChatGPT.revChatGPT import Chatbot
+import os
+import re
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from dotenv import load_dotenv
+load_dotenv()
+
+config = {
+    "email": os.getenv('EMAIL'),
+    "password": os.getenv('PASSWORD')
+}
+
+global chatbot
+chatbot = Chatbot(config, conversation_id=None)
+chatbot.refresh_session()
+
+# ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
+app = App(token=os.getenv('SLACK_BOT_TOKEN'))
+
+@app.message(re.compile(r"^!gpt (.*)$"))
+def message_img(client, message, say, context):
+    try:
+        prompt = context['matches'][0]
+        response = chatbot.get_chat_response(prompt)
+        print(f"prompt: `{prompt}`")
+        message = response['message']
+        print(f"response: `{response}`")
+        say(message)
+    except Exception as e:
+        print(e)
+        say(f"エラーが発生しました。やり方を変えて試してみてください。 Error: {e}")
+
+
+@app.message(re.compile(r"^!gpt-help$"))
+def message_help(client, message, say, context):
+    say("`!gpt [ボットに伝えたいメッセージ]` の形式でChatGPTのAIと会話できます")
+
+@app.event("message")
+def handle_message_events(body, logger):
+    logger.info(body)
+
+# アプリを起動します
+if __name__ == "__main__":
+    SocketModeHandler(app, os.getenv('SLACK_APP_TOKEN')).start()
